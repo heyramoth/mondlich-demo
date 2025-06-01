32636f49634c98453465eafcfa52be2197087804
Elizaveta Zalomnova <handlessdude@gmail.com>
2025-06-01 06:20:21 +0300

    update mondlich version

--- src/application/setupDemoScene.ts
+++ src/application/setupDemoScene.ts
@@ -48,7 +48,9 @@ export const setupDemoScene = async (): Promise<void> => {
 
   await manager.textureManager.loadTextureLibrary();
   const firework = manager.createFirework({ particlesCount: 20000 });
-  firework.settings.origin = [0, 0, -500];
+  const firework2 = manager.createFirework({ particlesCount: 20000 });
+  firework.settings.origin = [0, 0, -1000];
+  manager.setWorkerEnabled(firework, true);
 
   const timer = new Timer(false);
 
@@ -59,7 +61,7 @@ export const setupDemoScene = async (): Promise<void> => {
       point: firework.settings.origin,
       axisOrigin: [0, 0, 0],
       axisDirection: [0, 1, 0],
-      rotationAngle: elapsedTime,
+      rotationAngle: elapsedTime * 0.25,
     });
   };
 
@@ -76,10 +78,11 @@ export const setupDemoScene = async (): Promise<void> => {
 
   timer.start();
   firework.start();
+  firework2.start();
 
-  const loop = (): void => {
+  const loop = async () => {
     updateFireworkSettings();
-    manager.update();
+    await manager.update();
     userInput.update();
 
 
--- vite.config.mts
+++ vite.config.mts
@@ -10,10 +10,20 @@ export default defineConfig({
     plugins: [eslint()],
     build: {
         emptyOutDir: true,
+        assetsDir: 'assets',
     },
     server: {
         port: 3000,
         open: true,
+        fs: {
+            allow: [
+                '.',
+                './node_modules/mondlich/dist',
+            ],
+        },
+    },
+    optimizeDeps: {
+        exclude: ['mondlich']
     },
     resolve: {
         alias: {
